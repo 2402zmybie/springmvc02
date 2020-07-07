678d660b4e84d106ecc78e5e41d9bfac7515ea03
2402zmybie <2402368990@qq.com>
2020-07-07 20:35:53 +0800

    fileupload包的传统方式实现文件上传

---
+++ 02fileupload/src/main/java/com/hr/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.hr.controller;
+
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/fileupload1")
+    public String fileupload1(HttpServletRequest request) throws Exception {
+        System.out.println("文件上传...");
+
+        //使用fileupload组件完成文件上传
+
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        File file = new File(path);
+        if(!file.exists()) {
+            file.mkdirs();
+        }
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        List<FileItem> items = upload.parseRequest(request);
+        for(FileItem item : items) {
+            //判断当前item对象是不是上传文件项(可能是表单项)
+            if(item.isFormField()) {
+                //表单项
+            }else {
+                //上传文件项
+                //获取上传文件的名称
+                String fileName = item.getName();
+                //把文件的名称设置为唯一的值
+                String uuid = UUID.randomUUID().toString().replace("-", "");
+                fileName = uuid + "_" + fileName;
+                //完成文件上传
+                item.write(new File(path,fileName));
+                //删除临时文件
+                item.delete();
+            }
+        }
+        return "success";
+    }
+
+
+
+}
---
+++ 02fileupload/src/main/webapp/WEB-INF/pages/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Gatsby
+  Date: 2020/7/7
+  Time: 20:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h3>上传文件成功</h3>
+</body>
+</html>
