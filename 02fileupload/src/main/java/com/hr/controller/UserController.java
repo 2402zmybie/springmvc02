3f64f8eec687ff443cd3067f01e92c81d54e123c
Lc <641229781@qq.com>
2020-07-08 10:46:57 +0800

    springmvc实现文件上传

--- 02fileupload/src/main/java/com/hr/controller/UserController.java
+++ 02fileupload/src/main/java/com/hr/controller/UserController.java
@@ -7,6 +7,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -55,6 +56,24 @@ public class UserController {
      * SpringMvc提供了MultipartFile对象,该对象表示上传的文件,要求变量名称必须和表单file标签的name属性名称相同
      *
      */
+    @RequestMapping("/fileupload2")
+    public String fileupload2(HttpServletRequest request, MultipartFile upload) throws Exception {
+        System.out.println("springmvc文件上传...");
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        File file = new File(path);
+        if(!file.exists()) {
+            file.mkdirs();
+        }
+
+        String fileName = upload.getOriginalFilename();
+        //把文件的名称设置为唯一的值
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        fileName = uuid + "_" + fileName;
+        //完成文件上传
+        upload.transferTo(new File(path,fileName));
+
+        return "success";
+    }
 
 
 
--- 02fileupload/src/main/webapp/index.jsp
+++ 02fileupload/src/main/webapp/index.jsp
@@ -12,7 +12,18 @@
 </head>
 <body>
 
+    <h3>传统文件上传</h3>
     <form action="user/fileupload1" method="post" enctype="multipart/form-data">
+        选择文件: <input type="file" name="upload"/> <br>
+        <input type="submit" value="上传">
+    </form>
+
+    <br>
+    <br>
+    <br>
+
+    <h3>Springmvc文件上传</h3>
+    <form action="user/fileupload2" method="post" enctype="multipart/form-data">
         选择文件: <input type="file" name="upload"/> <br>
         <input type="submit" value="上传">
     </form>
